a03bfc67f7ae1b5f5d4a132c1ffb305e4ba4b766
심승용 <seungyongshim@nexon.co.kr>
2023-04-12 15:36:24 +0900

    .

--- src/TestProject1/Tests.fs
+++ src/TestProject1/Tests.fs
@@ -10,3 +10,12 @@ let ``Cell인데 Covered인 경우`` () =
     let sut = Covered Mine
     Assert.Equal(debug sut, "*")
 
+[<Fact>]
+let ``Cell인데 Number인 경우`` () =
+    let sut = Number 1
+    Assert.Equal(debug sut, "1")
+
+[<Fact>]
+let ``Cell인데 Mine인 경우`` () =
+    let sut = Mine
+    Assert.Equal(debug sut, "*")
